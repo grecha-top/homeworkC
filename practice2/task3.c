b9ad69b0d89c14be1c8f09644f4e5f2b8889639f
grechatop <ugrechishnikova07@bk.ru>
2025-02-14 21:27:51 +0300

    replaced with dynamic array in task3

--- practice2/task3.c
+++ practice2/task3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Event {
     char event[50];
@@ -10,7 +11,7 @@ int main() {
     int n;
     printf("Enter to количество events: \n");
     scanf("%d", &n);
-    struct Event events[n];
+    struct Event *events = (struct Event*)malloc(sizeof(n));
     for (int i=0; i<n; i++) {
         scanf("%s %d %d %d %s", &events[i].event, &events[i].date[0], &events[i].date[1], &events[i].date[2], &events[i].description);
     }
@@ -19,5 +20,6 @@ int main() {
         printf("Date: %d/%d/%d\n", events[i].date[0], events[i].date[1], events[i].date[2]);
         printf("Description: %s\n", events[i].description);
     }
+    free(events);
     return 0;
 }
